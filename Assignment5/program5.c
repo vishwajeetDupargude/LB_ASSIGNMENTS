8bd150c97071a29bc02c35eaab912fa48cf3040d
vishwajeet <vishwajeetdupargude600@gmail.com>
2025-11-02 14:40:58 +0530

    pending

--- Assignment5/program5.c
+++ Assignment5/program5.c
@@ -1,1 +1,25 @@
-#inclus
+#include<stdio.h>
+int FindLargest(int x, int y, int z)
+{
+     if (x >= y && x >= z)
+    {
+        return x;
+    }
+    else if (y >= x && y >= z)
+    {
+        return y;
+    }
+    else
+    {
+        return z;
+    }    
+}
+int main()
+{
+    int a,b,c,result;
+    printf("Enter a three numbers");
+    scanf("%d %d %d",&a,&b,&c);
+    result = FindLargest(a,b,c);
+    printf("largest numbers %d:\n",result);
+    return 0;
+}
